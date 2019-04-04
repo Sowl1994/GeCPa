30690a178bac2a947f0415c06621be28d4c9b83b
Francisco Lopez <jflvilchez94@hotmail.com>
2019-04-04 19:14:42 +0200

    feat: Añadir productos a la deuda de un cliente

---
+++ src/Form/DebtFormType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Debt;
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DebtFormType extends AbstractType
+{
+
+    private $productRepository;
+    public function __construct(ProductRepository $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            //->add('quantity',NumberType::class,['label' => 'Cantidad', 'mapped' => false])
+            ->add('purchaseDate',DateType::class,['label' => 'Fecha de compra','format' => 'dd/MM/yyyy'])
+            /*->add('product',EntityType::class, [
+                'label'=>' ',
+                'class' => Product::class,
+                'choices'=> $this->productRepository->getActiveProducts(),
+                'choice_label' => function ($prod) {
+                    return $prod;
+                }
+            ])*/
+            /*->add('paymentDate',DateType::class,['label' => 'Fecha de pago'])
+            ->add('isPaid',CheckboxType::class,['label' => 'Pagado'])*/
+            /*->add('client',TextType::class,['mapped'=> false])
+            ->add('product',TextType::class,['mapped'=> false])*/
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Debt::class,
+        ]);
+    }
+}
