b5ba3cb6f14cc7e26c0d39e3df957ee458e285b8
Francisco Lopez <jflvilchez94@hotmail.com>
2019-04-05 09:47:00 +0200

    fix: calculate_debt limpiada

--- src/Controller/DebtController.php
+++ src/Controller/DebtController.php
@@ -119,8 +119,8 @@ class DebtController extends AbstractController
         }else{
             $bd = $debtRepository->getClientBreakdown($id);
 
-            //$count = $this->calculate_debt($id, $bd,new \DateTime('2019-04-02'),new \DateTime('2019-04-04'));
-            $count = $this->calculate_debt($id, $bd);
+            //$count = $this->calculate_debt($bd,new \DateTime('2019-04-02'),new \DateTime('2019-04-02'));
+            $count = $this->calculate_debt($bd);
         }
 
 
@@ -132,32 +132,23 @@ class DebtController extends AbstractController
     }
 
     /**
-     * @param $id
      * @param $bd
      * @param null $date1
      * @param null $date2
      * @return float
      */
-    public function calculate_debt($id, $bd, $date1 = null, $date2 = null ):float {
+    public function calculate_debt($bd, $date1 = null, $date2 = null ):float {
         $count = 0;
-        if ($date1 != null && $date2 != null){
-            foreach($bd as $product){
-                if ($product['purchaseDate'] >= $date1 && $product['purchaseDate'] <= $date2){
-                    $q = $product['quantity'];
-                    $p = $product['price'];
 
-                    $count += $q * $p;
-                }
+        foreach($bd as $product){
+            $q = $product['quantity'];
+            $p = $product['price'];
+            if ( ($date1 != null && $date2 != null) && ($product['purchaseDate'] < $date1 || $product['purchaseDate'] > $date2) ){
+                $q = $p = 0;
             }
-        }else{
-            foreach($bd as $product){
-                $q = $product['quantity'];
-                $p = $product['price'];
 
-                $count += $q * $p;
-            }
+            $count += $q * $p;
         }
-
         return $count;
     }
 }
