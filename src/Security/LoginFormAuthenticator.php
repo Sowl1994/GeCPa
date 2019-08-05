5455bc61977b8a2fa0667f42adbc4d563075a586
Francisco Lopez <jflvilchez94@hotmail.com>
2019-08-05 10:18:23 +0200

    fix: Eliminado comentario

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -93,7 +93,6 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
         }
 
         return new RedirectResponse($this->urlGenerator->generate("app_home"));
-        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl()
